5749952ede0e316e7c6cad9acfe481d9e7fbffde
Benjamin Brückner <69013582+BenNorsk@users.noreply.github.com>
2025-03-04 09:45:26 +0000

    Feat: Added some linear regressions for the Maike result

--- bullshit_jobs/bs_score_validation/_manual.py
+++ bullshit_jobs/bs_score_validation/_manual.py
@@ -1,6 +1,8 @@
 from bullshit_jobs.load_data._load_data import _quick_load, _save_data
 import pandas as pd
 import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
 
 
 def _select_random_sample() -> pd.DataFrame:
@@ -55,6 +57,62 @@ def _select_random_sample() -> pd.DataFrame:
 
     return sample_df
 
-if __name__ == "__main__":
-    df = _select_random_sample()
+def _evaluate_bs_scores_manually() -> pd.DataFrame:
+    """
+    Evaluates the bs_scores manually.
+
+    Returns:
+    --------
+    pd.DataFrame
+        The evaluated DataFrame.
+    """
+    # Load the data
+    df = _quick_load("data_with_bs_scores.pkl")
+    evaluation = pd.read_excel("./data/manual_evaluation/manual_evaluation_task_maike_filled_in.xlsx")
+    print(evaluation)
+
+    # Merge the data on review_id
+    df = pd.merge(df, evaluation, on="review_id")
+
+    # Drop all columns where score_maike is NaN
+    df = df.dropna(subset=["score_maike"])
     print(df)
+
+    # Show the correlation between score_maike and bs_score_llm
+    print(df["score_maike"].corr(df["bs_score_llm"]))
+    print(df["score_maike"].corr(df["bs_score_binary_dict"]))
+
+    y = df[["score_maike"]]
+    X = df[["bs_score_llm"]]
+
+    # Fit the linear regression model
+    X = sm.add_constant(X)
+    model = sm.OLS(y, X)
+    results = model.fit()
+
+    # Print a model summary
+    print(results.summary())
+
+    # Fit the linear regression model for bs_score_binary_dict
+    y = df[["score_maike"]]
+    X = df[["bs_score_binary_dict"]]
+    X = sm.add_constant(X)
+    model = sm.OLS(y, X)
+    results = model.fit()
+
+    # Print a model summary
+    print(results.summary())
+
+    # Make a scatter plot with the linear regression line
+    plt.scatter(df["bs_score_binary_dict"], df["score_maike"])
+    plt.plot(df["bs_score_binary_dict"], results.predict(X), color="red")
+    plt.xlabel("bs_score_binary_dict")
+    plt.ylabel("score_maike")
+    plt.savefig("./data/manual_evaluation/bs_score_binary_dict_vs_score_maike.png")
+
+    return
+
+if __name__ == "__main__":
+    # df = _select_random_sample()
+    # print(df)
+    _evaluate_bs_scores_manually()
